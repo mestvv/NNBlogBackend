f8577aaf758617eaa3ea02c70f7048f1613a59f1
MadFlop <mestvlad@gmail.com>
2024-05-13 00:24:13 +0900

    Исправление миграции

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -6,10 +6,25 @@ import (
 	"github.com/google/uuid"
 )
 
+type UserRole string
+
+const (
+	UserRoleDefault = "default"
+	UserRoleAdmin   = "administrator"
+	UserRoleModer   = "moderator"
+)
+
 type User struct {
-	ID    uuid.UUID `db:"id"`
-	Name  string    `db:"name"`
-	Email string    `db:"email"`
+	ID        uuid.UUID `db:"id"`
+	Password  string    `db:"password"`
+	Username  string    `db:"username"`
+	FirstName string    `db:"first_name"`
+	LastName  string    `db:"last_name"`
+	Email     string    `db:"email"`
+	Phone     string    `db:"phone"`
+	Bio       string    `db:"bio"`
+	Role      UserRole  `db:"role"`
+	Active    bool      `db:"active"`
 
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
--- migrations/20240512141406_user.sql
+++ migrations/20240512141406_user.sql
@@ -5,17 +5,24 @@ SELECT 'up SQL query';
 
 CREATE TABLE user (
     id binary(16) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL COMMENT 'Email пользователя',
-    phone VARCHAR(20) UNIQUE NOT NULL COMMENT 'Номер телефона',
     password VARCHAR(255) NOT NULL COMMENT 'Пароль',
+    username VARCHAR(255) UNIQUE NOT NULL COMMENT 'Ник пользователя',
+    first_name VARCHAR(255) DEFAULT NULL COMMENT 'Имя пользователя',
+    last_name VARCHAR(255) DEFAULT NULL COMMENT 'Фамилия пользователя',
+    email VARCHAR(255) UNIQUE NOT NULL COMMENT 'Email пользователя',
+    phone VARCHAR(20) UNIQUE DEFAULT NULL COMMENT 'Номер телефона',
+    bio VARCHAR(255) DEFAULT NULL COMMENT 'Биография пользователя',
+    role VARCHAR(255) NOT NULL COMMENT 'Роль пользователя',
+    active BOOL DEFAULT FALSE COMMENT 'Статус активизации аккаунта',
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at datetime DEFAULT NULL,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
 -- +goose StatementEnd
+
+DROP TABLE user;
